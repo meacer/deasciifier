c043bca174645a35987eb876c39d71591fda71c9
meacer@gmail.com <meacer@gmail.com@94af73e8-2eda-5aeb-0def-fc60e0e6ab0a>
2011-07-16 23:55:01 +0000

    Add missing file

---
+++ trunk/src/js/lib/turkish.chars.js
@@ -0,0 +1,16 @@
+//
+// List of Turkish characters
+//
+
+/** @const */  var DEASCII_TR_LOWER_C = '\u00E7';
+/** @const */  var DEASCII_TR_UPPER_C = '\u00C7';
+/** @const */  var DEASCII_TR_LOWER_G = '\u011F';
+/** @const */  var DEASCII_TR_UPPER_G = '\u011E';
+/** @const */  var DEASCII_TR_LOWER_I = '\u0131';
+/** @const */  var DEASCII_TR_UPPER_I = '\u0130';
+/** @const */  var DEASCII_TR_LOWER_O = '\u00F6';
+/** @const */  var DEASCII_TR_UPPER_O = '\u00D6';
+/** @const */  var DEASCII_TR_LOWER_S = '\u015F';
+/** @const */  var DEASCII_TR_UPPER_S = '\u015E';
+/** @const */  var DEASCII_TR_LOWER_U = '\u00FC';
+/** @const */  var DEASCII_TR_UPPER_U = '\u00DC';
