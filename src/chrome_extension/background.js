0296398ff7211a1e97075215ecbd6f6fb089ae5a
meacer <meacer@gmail.com>
2013-08-15 04:59:50 +0000

    Update Chrome extension.
    Remove host permissions
    Use activeTab API instead of running content scripts
    Fix popup delay
    Use commands API for handling shortcuts

--- src/chrome_extension/background.js
+++ src/chrome_extension/background.js
@@ -1,5 +1,4 @@
 var MSG_CONVERT_FULL_TEXT = "T\u00FCm metni \u00E7evirmek istedi\u011Finizden emin misiniz?";
-var MSG_SELECT_TEXT_TO_CONVERT = "T\u00FCrk\u00E7e karakterleri \u00E7evirmek i\u00E7in bir metin kutusu se\u00E7in.";
 var MSG_CHARS_REPLACED = " karakter de\u011Fi\u015Ftirildi";
 var MSG_AUTO_CONVERT_ON = "Otomatik \u00E7evrim A\u00C7IK";
 var MSG_AUTO_CONVERT_OFF = "Otomatik \u00E7evrim KAPALI";
@@ -10,11 +9,6 @@ var MSG_CONVERT_TURKISH = "T\u00FCrk\u00E7e karakterleri \u00E7evir (Deasciify)"
 
 // Deasciifier
 var deasciifier = Deasciifier;
-
-var deasciifyResult = {textFound:false};
-function getDeasciifyResult() { // used by popup.html
-  return deasciifyResult;
-}
 
 function deasciifyInput(input) {
   // Deasciify the text
@@ -31,7 +25,7 @@ function deasciifyInput(input) {
     result = MEA.HtmlDeasciifier.deasciify(text);
   } else {
     // Plain text
-    if (selectionStart!=selectionEnd) {
+    if (selectionStart != selectionEnd) {
       // only selected part
       result = deasciifier.deasciifyRange(text, selectionStart, selectionEnd);
     } else {
@@ -45,76 +39,104 @@ function deasciifyInput(input) {
   return result;
 }
 
-function convertSelectedEditableInTab(tab) {
-  if (!tab) {
-    return;
-  }
-  
-  var currentTabId = tab.id;
-  deasciifyResult.text = MSG_SELECT_TEXT_TO_CONVERT;
-  deasciifyResult.textFound = false;
-  
-  // Connect to the tab
-  var port = chrome.tabs.connect(currentTabId);
-  if (!port) {
-    window.console && console.log("Deasciifier - Could not connect to tab");
-    return;
-  }
-  
-  function messageListener(msg) {
-    if (msg.message=="TEXT_TO_DEASCIIFY") {
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  var msg = request;
+  switch (msg.message) {
+    case "TEXT_TO_DEASCIIFY":
+    {
       var result = deasciifyInput(msg.input);
       if (result) {
-        var num_toggled_chars = result.changedPositions ? result.changedPositions.length:0;
-        deasciifyResult.text = num_toggled_chars + MSG_CHARS_REPLACED;
-        deasciifyResult.textFound = true;
-        
-        // Send back the deasciified text:
-        port.postMessage({
-          message:"DEASCIIFIED_TEXT",
-          text: result.text,                        // deasciified text
-          selectionStart:msg.input.selectionStart,  // restore selection
-          selectionEnd:msg.input.selectionEnd
+        var toggledCharCnt =
+            result.changedPositions ? result.changedPositions.length : 0;
+        // Send back the result.
+        sendResponse({
+          message: "DEASCIIFIED_TEXT",
+          text: result.text,
+          selectionStart: msg.input.selectionStart,
+          selectionEnd: msg.input.selectionEnd
         });
-        // Show some information on the extension icon:
-        chrome.browserAction.setBadgeText({
-          'text':''+num_toggled_chars,
-          'tabId':currentTabId
-        });
-        chrome.browserAction.setTitle({
-          'title':num_toggled_chars + MSG_CHARS_REPLACED,
-          'tabId':currentTabId
-        });
+        setBrowserActionUI(currentTabId, toggledCharCnt);
       }
-      //port.disconnect();
     }
+    break;
+
+    case "NOTHING_TO_DEASCIIFY":
+      chrome.browserAction.setPopup({popup: "popup.html"});
+      break;
+
+    case "DEASCIIFY_HANDLER_ON":
+      showNotification(MSG_AUTO_CONVERT_ON,
+                       MSG_TEXT_WILL_BE_AUTO_CONVERTED, 3500);
+    break;
+
+    case "DEASCIIFY_HANDLER_OFF":
+      showNotification(MSG_AUTO_CONVERT_OFF,
+                       MSG_TEXT_WONT_BE_AUTO_CONVERTED, 3500);
+    break;
+
+    case "DEASCIIFY_TYPED_TEXT":
+    {
+      var text = msg.text;
+      if (text) {
+        var converted = null;
+        if (msg.selectionStart == msg.selectionEnd && msg.selectionStart > 0) {
+          // Cursor is in the middle of the text
+          converted = deasciify_word_at_cursor(text, msg.selectionStart);
+        } else {
+          converted = deasciifier.turkish_correct_last_word(text);
+        }
+        if (converted) {
+          // Send back deasciified text to content script.
+          sendResponse({
+            "text": converted.text,
+            "selectionStart": msg.selectionStart,
+            "selectionEnd": msg.selectionEnd
+          });
+        }
+      }
+    }
+    break;
   }
-  
-  port.onMessage.addListener(messageListener);
-  // Request the text to deasciify
-  port.postMessage({message: "REQUEST_TEXT"});
+});
+
+function setBrowserActionUI(currentTabId, toggledCharCnt) {
+  // Show some information on the extension icon:
+  chrome.browserAction.setBadgeText({
+    'text': '' + toggledCharCnt,
+    'tabId': currentTabId
+  });
+  chrome.browserAction.setTitle({
+    'title': toggledCharCnt + MSG_CHARS_REPLACED,
+    'tabId': currentTabId
+  });
 }
+
+function convertSelectedEditableInTab(tab) {
+  if (!tab) {
+    console.log("No tab");
+    return;
+  }
+  console.log("Browser action, tab id: " + tab.id);
+  chrome.tabs.executeScript(tab.id, {file: "execute.js"}, function() {
+    chrome.tabs.executeScript(tab.id, {code: "deasciifyActiveElement();"});
+  });
+};
 
 function ConvertTurkishChars() {
   // Get the current tab:
   chrome.tabs.getSelected(null, convertSelectedEditableInTab);
 }
 
-// Handle the messages from the content script on Alt+T key:
-chrome.extension.onConnect.addListener(messageListener);
-
 function showNotification(title, text, timeout) {
- if (window.webkitNotifications) {
-    var notification = webkitNotifications.createNotification(
-      ICON_NOTIFICATION,
-      title,           
-      text
-    );
-    notification.show();
-    setTimeout(function(){ notification.cancel(); }, timeout);
-  }
+  var notification = webkitNotifications.createNotification(
+    ICON_NOTIFICATION,
+    title,
+    text
+  );
+  notification.show();
+  setTimeout(function(){ notification.cancel(); }, timeout);
 }
- 
+
 function deasciify_word_at_cursor(text, cursorPos) {
   var wordBeforeCursor = MEA.TextHelper.getWordBeforeCursor(text, cursorPos);
   if (wordBeforeCursor) {
@@ -123,51 +145,10 @@ function deasciify_word_at_cursor(text, cursorPos) {
   return null;
 }
 
-function messageListener(port) {
-
-  if (port.name=="deasciify_on_typing") {
-  
-    function onMessageHandler(msg) {
-      switch (msg.message) {
-        case "DEASCIIFY_HANDLER_ON":
-          showNotification(MSG_AUTO_CONVERT_ON, MSG_TEXT_WILL_BE_AUTO_CONVERTED, 3500);
-        break;
-      
-        case "DEASCIIFY_HANDLER_OFF":
-          showNotification(MSG_AUTO_CONVERT_OFF, MSG_TEXT_WONT_BE_AUTO_CONVERTED, 3500);
-        break;
-        
-        case "DEASCIIFY_TYPED_TEXT":
-          var text = msg.text;
-          if (text && deasciifier && port) {
-            var converted = null;
-            if (msg.selectionStart==msg.selectionEnd && msg.selectionStart>0){ 
-              // Cursor is in the middle of the text
-              converted = deasciify_word_at_cursor(text, msg.selectionStart);
-            } else {
-              converted = deasciifier.turkish_correct_last_word(text);
-            }
-            if (converted) {
-              // send back deasciified text to content script
-              port.postMessage({
-                "message":"DEASCIIFIED_TEXT_ON_TYPING",
-                "text":converted.text,
-                "selectionStart":msg.selectionStart,
-                "selectionEnd":msg.selectionEnd
-              });
-            }
-          }
-        break;
-      }
-    }
-    port.onMessage.addListener(onMessageHandler);
-  }
-}
 // Called when the user clicks on the browser action.
-/*chrome.browserAction.onClicked.addListener(function(tab) {
+chrome.browserAction.onClicked.addListener(function(tab) {
   ConvertTurkishChars();
-});*/
-
+});
 
 function getContextMenuClickHandler() {
   return function(info, tab) {
@@ -175,8 +156,17 @@ function getContextMenuClickHandler() {
   }
 }
 chrome.contextMenus.create({
-  "title":MSG_CONVERT_TURKISH,
-  "type":"normal",
-  "contexts":["editable"],
-  "onclick":getContextMenuClickHandler()
+  "title": MSG_CONVERT_TURKISH,
+  "type": "normal",
+  "contexts": ["editable"],
+  "onclick": getContextMenuClickHandler()
+});
+
+chrome.commands.onCommand.addListener(function(command){
+  console.log("onCommand");
+  chrome.tabs.getCurrent(function(tab){
+    chrome.tabs.executeScript(null, {file: "execute.js"}, function() {
+      chrome.tabs.executeScript(null, {code: "toggleAutoDeasciify();"});
+    });
+  });
 });
