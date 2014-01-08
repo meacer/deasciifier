d1e8b48f907f81b203598cf75864253f012a38b7
meacer <meacer@94af73e8-2eda-5aeb-0def-fc60e0e6ab0a>
2014-01-08 01:39:22 +0000

    Convert Chrome extension version reader to python code, remove Java code.

---
+++ trunk/build/chrome_extension_version.py
@@ -0,0 +1,20 @@
+"""
+Reads the version information from the manifest of the Chrome extension.
+"""
+
+import json
+import sys
+
+
+def ReadChromeExtensionVersion(manifest_path):
+  with open(manifest_path) as manifest_file:
+    manifest = json.load(manifest_file)
+    print(manifest['version'])
+
+
+if __name__ == "__main__":
+  if len(sys.argv) > 1:
+    ReadChromeExtensionVersion(sys.argv[1])
+  else:
+    print('\nUsage: chrome_extension_version.py <manifest_path>\n')
+    exit(-1)
