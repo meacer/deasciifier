e01cbf2f509309ce0cc07ad276c9282ed2930d02
meacer <meacer@gmail.com>
2014-12-20 18:43:34 +0000

    Add sample C# project.

---
+++ src/csharp/DeasciifierSampleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeasciifierSampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            org.deasciifier.Deasciifier deasciifier = new org.deasciifier.Deasciifier();
+            deasciifier.init();
+            Console.WriteLine(deasciifier.deasciify("turkce klavyesi olmayanlar icin bir yazilim."));
+        }
+    }
+}
