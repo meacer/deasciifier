08eda97d9b399f8a8b014c12b699b6760f249d9d
Mustafa Emre Acer <meacer@gmail.com>
2016-01-13 21:04:27 +0200

    Convert line endings to Unix

--- src/js/lib/turkish_encoder.js
+++ src/js/lib/turkish_encoder.js
@@ -1,52 +1,35 @@
-﻿// Turkish character encoder for HTML and JavaScript
-//
-// Author: Mustafa Emre Acer
-//
-// Converts Turkish characters to HTML and JavaScript codes
-//
-
-var TurkishEncoder = {};
-
-(function(){
+﻿(function(exports){
 
   /** @enum {string} */
   var TURKISH_HTML_TABLE = {
-    '\u00E7':'&#231;', // turkish lowercase c
-    '\u00C7':'&#199;', // turkish uppercase c
-    '\u011F':'&#287;', // turkish lowercase g
-    '\u011E':'&#286;', // turkish uppercase g
-    '\u0131':'&#305;', // turkish lowercase i
-    '\u0130':'&#304;', // turkish uppercase i
-    '\u00F6':'&#246;', // turkish lowercase o
-    '\u00D6':'&#214;', // turkish uppercase o
-    '\u015F':'&#351;', // turkish lowercase s
-    '\u015E':'&#350;', // turkish uppercase s
-    '\u00FC':'&#252;', // turkish lowercase u
-    '\u00DC':'&#220;'  // turkish uppercase u
+    '\u00E7': '&#231;', // turkish lowercase c
+    '\u00C7': '&#199;', // turkish uppercase c
+    '\u011F': '&#287;', // turkish lowercase g
+    '\u011E': '&#286;', // turkish uppercase g
+    '\u0131': '&#305;', // turkish lowercase i
+    '\u0130': '&#304;', // turkish uppercase i
+    '\u00F6': '&#246;', // turkish lowercase o
+    '\u00D6': '&#214;', // turkish uppercase o
+    '\u015F': '&#351;', // turkish lowercase s
+    '\u015E': '&#350;', // turkish uppercase s
+    '\u00FC': '&#252;', // turkish lowercase u
+    '\u00DC': '&#220;'  // turkish uppercase u
   };
 
   /** @enum {string} */
   var TURKISH_JS_TABLE = {
-    '\u00E7':'\\u00E7', // turkish lowercase c
-    '\u00C7':'\\u00C7', // turkish uppercase c
-    '\u011F':'\\u011F', // turkish lowercase g
-    '\u011E':'\\u011E', // turkish uppercase g
-    '\u0131':'\\u0131', // turkish lowercase i
-    '\u0130':'\\u0130', // turkish uppercase i
-    '\u00F6':'\\u00F6', // turkish lowercase o
-    '\u00D6':'\\u00D6', // turkish uppercase o
-    '\u015F':'\\u015F', // turkish lowercase s
-    '\u015E':'\\u015E', // turkish uppercase s
-    '\u00FC':'\\u00FC', // turkish lowercase u
-    '\u00DC':'\\u00DC'  // turkish uppercase u
-  };
-
-  TurkishEncoder.encodeHTML = function(str) {
-    return encode(str, TURKISH_HTML_TABLE);
-  };
-
-  TurkishEncoder.encodeJS = function(str) {
-    return encode(str, TURKISH_JS_TABLE);
+    '\u00E7': '\\u00E7', // turkish lowercase c
+    '\u00C7': '\\u00C7', // turkish uppercase c
+    '\u011F': '\\u011F', // turkish lowercase g
+    '\u011E': '\\u011E', // turkish uppercase g
+    '\u0131': '\\u0131', // turkish lowercase i
+    '\u0130': '\\u0130', // turkish uppercase i
+    '\u00F6': '\\u00F6', // turkish lowercase o
+    '\u00D6': '\\u00D6', // turkish uppercase o
+    '\u015F': '\\u015F', // turkish lowercase s
+    '\u015E': '\\u015E', // turkish uppercase s
+    '\u00FC': '\\u00FC', // turkish lowercase u
+    '\u00DC': '\\u00DC'  // turkish uppercase u
   };
 
   function encode(text, char_table) {
@@ -64,9 +47,17 @@ var TurkishEncoder = {};
     return {"text": output.join("")};
   }
 
-})();
+  /** @const */
+  var TurkishEncoder = {};
 
-// Exports for Closure Compiler
-window["TurkishEncoder"] = TurkishEncoder;
-window["TurkishEncoder"]["encodeJS"] = TurkishEncoder.encodeJS;
-window["TurkishEncoder"]["encodeHTML"] = TurkishEncoder.encodeHTML;
+  TurkishEncoder.encodeHTML = function(str) {
+    return encode(str, TURKISH_HTML_TABLE);
+  };
+
+  TurkishEncoder.encodeJS = function(str) {
+    return encode(str, TURKISH_JS_TABLE);
+  };
+
+  exports.TurkishEncoder = TurkishEncoder;
+
+})(exports);
