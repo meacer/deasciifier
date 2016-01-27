ba1e92607206eef36d147536ffd3c9e8f2177459
Mustafa Emre Acer <meacer@gmail.com>
2016-01-26 21:59:41 -0800

    Fix build script for the website

---
+++ src/js/lib/cc_exports/externs.js
@@ -0,0 +1,14 @@
+// Externs for Closure Compiler.
+var Deasciifier;
+
+Deasciifier.init = function(patternList) {};
+
+Deasciifier.deasciifyRange = function(text, start, end, options) {};
+
+Deasciifier.deasciify = function(text, options) {};
+
+var Asciifier;
+
+Asciifier.asciify = function(text) {};
+
+Asciifier.asciifyRange = function(text, start, end) {};
