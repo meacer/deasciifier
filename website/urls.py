203afe49fc3c831360da76c0b09a03bdb0f5352e
M <meacer@gmail.com>
2016-12-25 00:20:21 -0800

    Point /codemirror to the html file

--- website/urls.py
+++ website/urls.py
@@ -5,6 +5,8 @@ from django.conf.urls import patterns
 from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
+  (r'codemirror', TemplateView.as_view(template_name='index_codemirror.html')),
+
   (r'.*', TemplateView.as_view(template_name='index.html')),
-  (r'^$', TemplateView.as_view(template_name='index.html'))
+  (r'^$', TemplateView.as_view(template_name='index.html')),
 )
