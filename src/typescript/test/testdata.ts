461970063e2ab65593d0d2f9ab2080c290d2a960
M <meacer@gmail.com>
2016-12-25 00:02:45 -0800

    Add missing test/testdata.ts file

---
+++ src/typescript/test/testdata.ts
@@ -0,0 +1,30 @@
+export const TEST_DATA = [
+  {
+    deasciified:
+        'T\u00FCrk\u00E7e veya T\u00FCrkiye T\u00FCrk\u00E7esi, varl\u0131\u011F\u0131 ' +
+            'tam olarak ispatlanamam\u0131\u015F ortak Altay dil ailesine ba\u011Fl\u0131 ' +
+            'T\u00FCrk dillerinin O\u011Fuz \u00F6be\u011Fine \u00FCye bir dildir. T\u00FCrk ' +
+            'dilleri ailesi b\u00FCnyesindeki O\u011Fuz \u00F6be\u011Finde bulunur. ' +
+            'T\u00FCrk\u00E7e d\u00FCnyada en fazla konu\u015Fulan 15. dildir.',
+
+    asciified:
+        'Turkce veya Turkiye Turkcesi, varligi tam olarak ispatlanamamis ortak Altay ' +
+            'dil ailesine bagli Turk dillerinin Oguz obegine uye bir dildir. Turk dilleri ' +
+            'ailesi bunyesindeki Oguz obeginde bulunur. Turkce dunyada en fazla konusulan ' +
+            '15. dildir.',
+
+    js_encoded:
+        'T\\u00FCrk\\u00E7e veya T\\u00FCrkiye T\\u00FCrk\\u00E7esi, varl\\u0131\\u011F\\u0131 ' +
+            'tam olarak ispatlanamam\\u0131\\u015F ortak Altay dil ailesine ba\\u011Fl\\u0131 ' +
+            'T\\u00FCrk dillerinin O\\u011Fuz \\u00F6be\\u011Fine \\u00FCye bir dildir. T\\u00FCrk ' +
+            'dilleri ailesi b\\u00FCnyesindeki O\\u011Fuz \\u00F6be\\u011Finde bulunur. ' +
+            'T\\u00FCrk\\u00E7e d\\u00FCnyada en fazla konu\\u015Fulan 15. dildir.',
+
+    html_encoded:
+        'T&#252;rk&#231;e veya T&#252;rkiye T&#252;rk&#231;esi, varl&#305;&#287;&#305; ' +
+            'tam olarak ispatlanamam&#305;&#351; ortak Altay dil ailesine ba&#287;l&#305; ' +
+            'T&#252;rk dillerinin O&#287;uz &#246;be&#287;ine &#252;ye bir dildir. T&#252;rk ' +
+            'dilleri ailesi b&#252;nyesindeki O&#287;uz &#246;be&#287;inde bulunur. ' +
+            'T&#252;rk&#231;e d&#252;nyada en fazla konu&#351;ulan 15. dildir.'
+  }
+];
