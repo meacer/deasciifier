774bc2884639ae47d24a5a7d2eeafceca97e7a6e
M <meacer@gmail.com>
2016-12-29 20:28:50 -0800

    Add protractor tests

---
+++ src/typescript/interactive_test/interactive_browser_test.ts
@@ -0,0 +1,74 @@
+import { App } from '../app';
+
+import protractor = require('protractor');
+import codemirror = require('codemirror');
+
+import chai = require('chai');
+let assert = chai.assert;
+let expect = chai.expect;
+let browser = protractor.browser;
+let element = protractor.element;
+let by = protractor.by;
+
+const PATTERNS = {
+  'c': 'aXa|-bXb|-aX|-Xa',
+  'g': 'aXa|-bX|-Xb',
+  'i': 'Xk'
+};
+
+describe('App', function () {
+  beforeEach(function() {
+    //browser.get('http://localhost:4444/codemirror');
+    browser.get('about:blank');
+    browser.executeScript("window.navigator = {}");
+  });
+
+  it('should test', function() {
+    //browser.executeScript("var editor = document.getElementById('txt').CodeMirror;editor.setValue('{');");
+    browser.executeScript(`
+      //var cm = CodeMirror(document.body);
+      var txt = document.createElement("textarea");
+      document.body.appendChild(txt);
+      var editor = CodeMirror.fromTextArea(document.getElementById("txt"), {
+        lineNumbers: false,
+        viewportMargin: Infinity,
+        lineWrapping: true,
+        minHeight: 900,
+        height: "dynamic",
+        autofocus: true
+      });
+      //var cm = CodeMirror(document.body, {
+      //value: "function myScript(){return 100;}\n",
+      //mode:  "javascript"
+      //});
+      */
+    `);
+/*
+    let textarea = document.createElement("textarea");
+    textarea.id = "txt";
+    document.body.appendChild(textarea);
+
+    let cm = codemirror.fromTextArea(textarea);
+    let div = document.createElement("div");
+    let app = new App(cm, PATTERNS, div);
+*/
+
+/*
+
+    element(by.id("txt")).sendKeys("this is a test");
+  assert.equal("tasdasldkj ", cm.getValue());*/
+
+    //element(by.model('todoList.todoText')).sendKeys('write first protractor test');
+    //element(by.css('[value="add"]')).click();
+
+    //var todoList = element.all(by.repeater('todo in todoList.todos'));
+    //expect(todoList.count()).toEqual(3);
+    //expect(todoList.get(2).getText()).toEqual('write first protractor test');
+
+    // You wrote your first test, cross it off the list
+    /*todoList.get(2).element(by.css('input')).click();
+    var completedAmount = element.all(by.css('.done-true'));
+    expect(completedAmount.count()).toEqual(2);
+    */
+  });
+});
---
+++ src/typescript/interactive_test/protractor.conf.js
@@ -0,0 +1,4 @@
+exports.config = {
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['../out/interactive_test/interactive_browser_test.js']
+};
