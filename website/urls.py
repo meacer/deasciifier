0cb96c0433b93fe43dc2eff1e5a9cd1b6bc478d1
M <meacer@gmail.com>
2017-12-31 12:10:59 +0300

    Fix url

--- website/urls.py
+++ website/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls import patterns
 from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
-  (r'v2', TemplateView.as_view(template_name='index_codemirror.html')),
+  (r'^v2', TemplateView.as_view(template_name='index_codemirror.html')),
 
   (r'.*', TemplateView.as_view(template_name='index.html')),
   (r'^$', TemplateView.as_view(template_name='index.html')),
