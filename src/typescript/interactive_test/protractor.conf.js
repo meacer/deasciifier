7a021ad684c3c72d6a1e7126db23336402463c93
meacer <meacer@gmail.com>
2020-09-20 01:25:42 -0700

    Remove webdriver from protractor tests, fix interactive-test on Windows

--- src/typescript/interactive_test/protractor.conf.js
+++ src/typescript/interactive_test/protractor.conf.js
@@ -1,9 +1,15 @@
+if (process.platform == "win32") {
+  process.env.CHROME_DRIVER = process.env.CHROME_DRIVER || "../node_modules/chromedriver/lib/chromedriver/chromedriver.exe"
+} else {
+  process.env.CHROME_DRIVER = process.env.CHROME_DRIVER || "../node_modules/chromedriver/bin/chromedriver"
+}
+
 exports.config = {
-  seleniumServerJar: "../node_modules/webdriver-manager/selenium/selenium-server-standalone-3.141.59.jar",
-  chromeDriver: "../node_modules/chromedriver/bin/chromedriver",
+  chromeDriver: process.env.CHROME_DRIVER,
   specs: ['../out/interactive_browser_test.js'],
 
   rootElement: 'html',
+  directConnect: true,
 
   // Root directory is typescript root.
   baseUrl: "file://" + __dirname + "/../",
@@ -12,7 +18,7 @@ exports.config = {
   capabilities: {
     browserName: 'chrome',
       chromeOptions: {
-        args: ['allow-file-access-from-files']
+        args: ['allow-file-access-from-files', '--headless']
       }
   },
 };
